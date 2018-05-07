bb8f02f02dfd9b2c237ddd184579c5bec2364343
rgreengo <35869907+rgreengo@users.noreply.github.com>
2018-05-07 16:20:09 -0300

    Add files via upload

--- WebApplicationSafeWeb/Historico.aspx.cs
+++ WebApplicationSafeWeb/Historico.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +14,61 @@ namespace WebApplicationSafeWeb
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+            if (!Page.IsPostBack)
+            {
+
+                if (Request.QueryString["id"] != null)
+                {
+                    lblID.Text = Request.QueryString["id"].ToString();
+
+
+                    string strcon = SqlDataSourceHistorico.ConnectionString.ToString();
+                    SqlConnection conexao = new SqlConnection(strcon);
+                    try
+                    {
+                        SqlCommand cmd = new SqlCommand("SELECT tbl_proposta_historico.Id, tbl_proposta_historico.id_usuario, tbl_proposta_historico.id_proposta, tbl_proposta_historico.data_aprovacao, tbl_proposta_historico.acao_realizada, tbl_usuarios.nome, tbl_propostas2.descricao, tbl_propostas2.valor, tbl_acao.acao, tbl_propostas2.nome AS propostaNome FROM tbl_proposta_historico INNER JOIN tbl_usuarios ON tbl_proposta_historico.id_usuario = tbl_usuarios.Id INNER JOIN tbl_propostas2 ON tbl_proposta_historico.id_proposta = tbl_propostas2.Id AND tbl_usuarios.Id = tbl_propostas2.usuario INNER JOIN tbl_acao ON tbl_proposta_historico.acao_realizada = tbl_acao.Id where tbl_proposta_historico.Id = @id", conexao);
+                        cmd.Parameters.Add("@id", SqlDbType.Int).Value = Request.QueryString["id"];
+                        SqlDataReader dr = null;
+
+                        conexao.Open();
+                        dr = cmd.ExecuteReader();
+
+
+
+                        while (dr.Read())
+                        {
+                            txtNome.Text = dr["nome"].ToString();
+                            txtAcao.Text = dr["acao"].ToString();
+                            txtValorProposta.Text = dr["valor"].ToString();
+                            txtDataModificacao.Text = dr["data_aprovacao"].ToString();
+                        }
+
+
+                        formCadastro.Visible = true;
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        throw;
+                    }
+                    finally
+                    {
+                        conexao.Close();
+
+                    }
+
+                }
+
+
+            }
+
+        }
+
+        protected void btnVoltar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
         }
     }
 }
--- WebApplicationSafeWeb/Historico.aspx.designer.cs
+++ WebApplicationSafeWeb/Historico.aspx.designer.cs
@@ -11,5 +11,113 @@ namespace WebApplicationSafeWeb {
     
     
     public partial class Historico {
+        
+        /// <summary>
+        /// Controle gridDiv.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl gridDiv;
+        
+        /// <summary>
+        /// Controle btnVoltar.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVoltar;
+        
+        /// <summary>
+        /// Controle rptHistorico.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptHistorico;
+        
+        /// <summary>
+        /// Controle SqlDataSourceHistorico.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSourceHistorico;
+        
+        /// <summary>
+        /// Controle formCadastro.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl formCadastro;
+        
+        /// <summary>
+        /// Controle lblID.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblID;
+        
+        /// <summary>
+        /// Controle RequiredFieldValidatorNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidatorNome;
+        
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
+        
+        /// <summary>
+        /// Controle txtAcao.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAcao;
+        
+        /// <summary>
+        /// Controle txtValorProposta.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtValorProposta;
+        
+        /// <summary>
+        /// Controle txtDataModificacao.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDataModificacao;
+        
+        /// <summary>
+        /// Controle lblInfo.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblInfo;
     }
 }
